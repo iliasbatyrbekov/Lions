2c9b98e55e5487e1c08c65ce6e00d4562734aa2c
luKE <lukehsu1999@gmail.com>
2020-10-18 17:29:59 +0800

    viewexpensebycategory

---
+++ src/main/Expense.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Expense extends Transaction {
+	private String category;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+}
--- src/main/View.java
+++ src/main/View.java
@@ -41,7 +41,22 @@ public class View {
 		}
 	}
 	public void viewExpensebyCategory() {
+		HashMap<String, Double> cat_exp = new HashMap<String, Double>();
 		
+		//BAD CODE SMELL: maybe consider recording transactions history in Accounts too
+		for (Transaction tra: tranHist) {
+			
+			//BAD DESIGN: transaction should'nt know that its children class "expense" has attribute "category"
+			if(tra.getType().equals("Expense")) {
+				String cat=((Expense)tra).getCategory();
+				cat_exp.put(cat,cat_exp.get(cat)+tra.getAmount());
+			}
+		}
+		
+		//print
+		for (String cat:cat_exp.keySet()) {
+			System.out.println(cat+" : "+cat_exp.get(cat));
+		}
 	}
 	public void viewExpensebyMember() {
 		
