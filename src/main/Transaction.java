83ca5650ed3f73c10d868ee47ff5830ef952d229
xavhl <50361174+xavhl@users.noreply.github.com>
2020-10-18 19:21:43 +0800

    update feature1

--- src/main/Transaction.java
+++ src/main/Transaction.java
@@ -1,28 +1,60 @@
 package main;
 
+import java.sql.Date;
+
 public class Transaction {
+	private String transactionID;
 	private double amount;
+	private Date date;
 	private String type;
 	private String accountID;
-	
-	public double getAmount() {
-		return amount;
+	private String description;
+
+	public Transaction(String transactionID) {
+		this.transactionID = transactionID;
 	}
+
+	public String getTransactionID() {
+		return this.transactionID;
+	}
+	
+	public String getDate() {
+		return this.date.toString();
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getAmount() {
+		return this.amount;
+	}
+
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
+
 	public String getType() {
-		return type;
+		return this.type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getAccount() {
-		return accountID;
+		return this.accountID;
 	}
+
 	public void setAccount(String accID) {
 		this.accountID = accID;
-	}
-	
-	
+	}	
 }
--- src/main/User.java
+++ src/main/User.java
@@ -5,30 +5,75 @@ import java.util.ArrayList;
 public class User {
 	private String userID;
 	private String password;
+	private int transactionIDGenerator;
 	private ArrayList<Transaction> transactionRecords;
 	private ArrayList<Account> accountList;
+	private ArrayList<Plan> planList;
 	private View view;
 	
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
+	public User(String userID, String password) {
 		this.userID = userID;
+		this.password = password;
+		this.transactionIDGenerator = 1;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
+
+	public void changePassword(String password) {
+		// pretend having done a series of complex verification, e.g. One Time Password, email confirmation
 		this.password = password;
 	}
-	public ArrayList<Transaction> getTransactionRecords() {
-		return transactionRecords;
+
+	// *** userID and password as confidential data should not have getter and setter for public modification ***
+	// public String getUserID() {
+	// 	return userID;
+	// }
+	// public void setUserID(String userID) {
+	// 	this.userID = userID;
+	// }
+	// public String getPassword() {
+	// 	return password;
+	// }
+	// public void setPassword(String password) {
+	// 	this.password = password;
+	// }
+	//*** View class already have transactionRecords ***
+	// public ArrayList<Transaction> getTransactionRecords() {
+	// 	return transactionRecords;
+	// }
+	// public void setTransactionRecords(ArrayList<Transaction> transactionRecords) {
+	// 	this.transactionRecords = transactionRecords;
+	// }
+
+	public void addTransaction() {
+		String transactionID = Integer.toString(this.transactionIDGenerator);
+		this.transactionIDGenerator += 1;
+
+		Transaction transaction = new Transaction(transactionID);
+		this.transactionRecords.add(transaction);
 	}
-	public void setTransactionRecords(ArrayList<Transaction> transactionRecords) {
-		this.transactionRecords = transactionRecords;
+
+	public void editTransaction(String transactionID) {
+		Transaction target = null;
+
+		for (Transaction transaction : this.transactionRecords) {
+			if(transaction.getTransactionID().equals(transactionID)) {
+				target = transaction;
+			}
+		}
+
+		if (target == null) { //throw Exception
+			System.out.println("Transaction Not Found");
+		}
+		else {
+			//To be continued
+			//target.setDate(...)
+			//target.setAmount(...)
+			//target.setType(...)
+			//target.setAccount(...)			
+		}
 	}
+
 	public ArrayList<Account> getAccountList() {
-		return accountList;
+		return this.accountList;
 	}
 	public void setAccountList(ArrayList<Account> accountList) {
 		this.accountList = accountList;
