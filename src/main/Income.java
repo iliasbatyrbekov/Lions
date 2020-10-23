318a26f25743a9a6f0b9d1b7abae10f98f912d79
Bryan <bryantjut@baisedu.org>
2020-10-24 01:19:35 +0800

    Auto stash before rebase of "origin/master"

---
+++ src/main/Income.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Income extends Transaction {
+	private String category;
+	private String member;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getMember() {
+		return member;
+	}
+
+	public void setMember(String member) {
+		this.member = member;
+	}
+
+}
