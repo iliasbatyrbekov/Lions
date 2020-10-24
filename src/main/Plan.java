c206f215ce66bed7426f4da4a2c0e9de1622828e
iKraken <ibatyrbek2-c@my.cityu.edu.hk>
2020-10-24 17:20:34 +0800

    add Plan

--- src/main/Plan.java
+++ src/main/Plan.java
@@ -1,5 +1,36 @@
 package main;
 
-public class Plan {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Plan {
+	private String planID;
+	private String planName;
+	private ArrayList<Date> timePeriod;
+	
+	private static int idGenSeed = 0;
+	
+	public Plan(String planName, ArrayList<Date> timePeriod) {
+		this.timePeriod = (ArrayList<Date>) timePeriod.clone();
+		this.planName = planName;
+		this.planID = Integer.toString(idGenSeed++);
+	}
+	
+	public String getPlanID() {
+		return planID;
+	}
+	
+	public String getPlanName() {
+		return planName;
+	}
+	public void setPlanName(String planName) {
+		this.planName = planName;
+	}
+	public ArrayList<Date> getTimePeriod() {
+		return timePeriod;
+	}
+	public void setTimePeriod(ArrayList<Date> timePeriod) {
+		this.timePeriod = timePeriod;
+	}
+	
 }
