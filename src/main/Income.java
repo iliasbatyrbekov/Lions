c6dad0335734193bb983ad4c66676e09340da281
ryanyen2 <ryanyen2-c@my.cityu.edu.hk>
2020-10-24 18:39:40 +0800

    add transaction history implementation

--- src/main/Income.java
+++ src/main/Income.java
@@ -3,8 +3,8 @@ package main;
 import java.sql.Date;
 
 public class Income extends Transaction {
-	public Income(String transactionID, Date date, double amount, String accountID, String description) {
-		super(transactionID, date, amount, accountID, description);
+	public Income(int transactionID, double amount, String accountID, String description) {
+		super(transactionID, amount, accountID, description);
 	}
 
 	private String category;
