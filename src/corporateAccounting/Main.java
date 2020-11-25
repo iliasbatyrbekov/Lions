a79346220b68ad44b4556f562676c125f4f113b5
TAI Hsiang-Yu <hsiangtai2@ad.cityu.edu.hk>
2020-11-25 21:59:03 +0800

    change the name of commands so they make more sense

--- src/corporateAccounting/Main.java
+++ src/corporateAccounting/Main.java
@@ -15,16 +15,27 @@ public class Main {
 		
 		// TODO Auto-generated method stub
 		boolean programFinished = false;
-		Scanner in = new Scanner(new File("C:\\Users\\hsiangtai2\\Desktop\\Lions\\src\\corporateAccounting\\in.txt"));
+		//Scanner in = new Scanner(new File("C:\\Users\\hsiangtai2\\Desktop\\Lions\\src\\corporateAccounting\\in.txt"));
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to our accounting system. It provides functions such as recording tractions, inventory purchases, recording inventory sales, viewing balance sheet/journal/storage, etc. Please specify which service you need by entering the corresponding command. Type \"help\" to see the full list of available commands.");
+		String allCommands = "==========================================================================\nREC: record transaction\n";
+		allCommands += "B: purchase inventory\n";
+		allCommands += "S: sell inventory\n";
+		allCommands += "J: view the journal (chronological record of all transactions)\n";
+		allCommands += "TL: view the T ledger of a specific account\n";
+		allCommands += "CLS: perform closing entry\n";
+		allCommands += "BS: view the balance sheet\n";
+		allCommands += "ST: view the storage entries\n";
+		allCommands += "Q: quit the program\n";
+		allCommands += "HELP: view the list of available commands\n==========================================================================";
 		while (!programFinished) {
-			System.out.println("Welcome to our accounting system. It provides three main functions, namely [A] recording tractions, [B] recording inventory purchases, and [C] recording inventory sales. Please specify which service you need by entering the corresponding letter [A/B/C]: ");
-			switch (in.next()) {
-				case "A":
+			System.out.print(">> ");
+			switch (in.next().toUpperCase()) {
+				case "REC":
 					System.out.println("Please enter the date of the transaction (dd/mm/yyyy), the account to debit, the account to credit, the amount to debit/credit, and a short description regarding the transaction: ");
 					CompanyTransaction transaction = new CompanyTransaction(myCompany.generateNewID(), new SimpleDateFormat("dd/MM/yyyy").parse(in.next()), in.next(), in.next(), Double.parseDouble(in.next()), in.nextLine());
 					if (myCompany.isValidTransaction(transaction)) {
 						myCompany.recordTransaction(transaction);
-						System.out.println("Transaction successfully added.");
 					} else {
 						System.out.println("Invalid transaction.");
 					}
@@ -33,32 +44,37 @@ public class Main {
 					System.out.println("Please enter the unit cost, units to buy, the date of the purchase, and the account to credit: ");
 					myCompany.purchaseInventory(in.nextDouble(), in.nextInt(), new SimpleDateFormat("dd/MM/yyyy").parse(in.next()), in.next());
 					break;
-				case "C":
+				case "S":
 					System.out.println("Please enter the unit price, units to sell, the date of the sale, the account to debit, and the cost method (LIFO/FIFO): ");
 					myCompany.sellInventory(in.nextDouble(), in.nextInt(), new SimpleDateFormat("dd/MM/yyyy").parse(in.next()), in.next(), in.next());
 					break;
-				case "D":
+				case "J":
 					myCompany.printJournal();
 					break;
-				case "E":
+				case "TL":
 					System.out.println("Please enter the name of the account to print");
 					myCompany.getAccountList().get(in.next()).printTLedger();
 					break;
-				case "F":
+				case "CLS":
 					System.out.println("Please enter the date to perform closing entry: ");
 					myCompany.generateClosingEntries(new SimpleDateFormat("dd/MM/yyyy").parse(in.next()));
 					break;
-				case "G":
+				case "BS":
 					myCompany.printBalanceSheet();
 					break;
-				case "H":
+				case "ST":
 					myCompany.printStorage();
 					break;
-				case "Z":
+				case "Q":
 					programFinished = true;
 					break;
-				//default:
-				//	System.out.println("Invalid input, please enter again [A/B/C]: ");
+				case "HELP":
+					System.out.println(allCommands);
+					break;
+				default:
+					System.out.println("Invalid input, please refer to the following list of available commands: ");
+					System.out.println(allCommands);
+					break;
 			}
 		}
 	}
